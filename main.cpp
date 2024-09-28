33fb715ad3d39ed051cbf81232eaf417229411e6
marissa <109318586+Marissacho@users.noreply.github.com>
2024-09-27 18:12:39 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,108 @@
+// This code was taken from https://www.techiedelight.com/queue-implementation-cpp/
+// The code has been modified from the original to provide opportunities to learn
+#include <iostream>
+#include "testqueue.hpp"
+#include <cstdlib>
+using namespace std;
+// define default capacity of the queue
+#define SIZE 10
+// Class for queue
+class queue
+{
+int* arr; // array to store queue elements
+int capacity; // maximum capacity of the queue
+int front; // front points to front element in the queue (if any)
+int rear; // rear points to last element in the queue
+int count; // current size of the queue
+public:
+queue(int size = SIZE); // constructor
+~queue(); // destructor
+void dequeue();
+void enqueue(int x);
+int peek();
+int size();
+bool isEmpty();
+bool isFull();
+}
+// Constructor to initialize queue
+queue::queue(int size)
+{
+arr = new int[size];
+capacity = size;
+front = 0;
+rear = -1;
+count = 0;
+}
+// Destructor to free memory allocated to the queue
+queue::~queue()
+{
+delete[] arr; // you are not required to test this function;
+// however, are there issues with it?
+}
+// Utility function to remove front element from the queue
+void queue::dequeue()
+{
+// check for queue underflow
+if (isEmpty())
+{
+cout << "UnderFlow\nProgram Terminated\n";
+return;
+}
+cout << "Removing " << arr[front] << '\n';
+front = (front + 1) % capacity;
+count--;
+}
+// Utility function to add an item to the queue
+void queue::enqueue(int item)
+{
+// check for queue overflow
+if (isFull())
+{
+cout << "OverFlow\nProgram Terminated\n";
+return;
+}
+cout << "Inserting " << item << '\n';
+rear = (rear + 1) % capacity;
+arr[rear] = item;
+count++;
+}
+// Utility function to return front element in the queue
+int queue::peek()
+{
+if (isEmpty())
+{
+cout << "UnderFlow\nProgram Terminated\n";
+return numeric_limits<int>::min();
+}
+return arr[front];
+}
+// Utility function to return the size of the queue
+int queue::size()
+{
+return count;
+}
+// Utility function to check if the queue is empty or not
+bool queue::isEmpty()
+{
+return (size() == 0);
+}
+// Utility function to check if the queue is full or not
+bool queue::isFull()
+{
+return (count == capacity);
+}
+// main function
+int main()
+{
+//poor attributes: commenting blocks( could have added more comments so the users know what the program is doing in depth, the structure of the different files could be better like making more files so my code is organized,my code could be more efficient in the main.cpp file, doesnt have exception handling, test cases could be more efficient.
+void testQueueSize(void);
+void testQueueIsEmpty(void);
+void testQueueIsFull(void);
+void testQueueDequeue(void);
+void testQueueEnqueue(void);
+void testQueuePeek(void);
+
+
+return 0;
+
+}
