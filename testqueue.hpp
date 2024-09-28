bc7b563d888220ddd1f9f29768b5c060ac90a203
marissa <109318586+Marissacho@users.noreply.github.com>
2024-09-27 18:13:17 -0700

    Create testqueue.hpp

---
+++ testqueue.hpp
@@ -0,0 +1,11 @@
+#ifndef TESTQUEUE_HPP 
+#define TESTQUEUE_HPP
+
+void testQueueSize(void);
+void testQueueIsEmpty(void);
+void testQueueIsFull(void);
+void testQueueDequeue(void);
+void testQueueEnqueue(void);
+void testQueuePeek(void);
+
+#endif
